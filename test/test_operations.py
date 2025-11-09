30f3783f34dc62420cc8f1f966551d1e2bd98444
sutariya <dishantsutariya24@gmail.com>
2025-11-09 13:56:31 +0100

    Add all necessary files

---
+++ test/test_operations.py
@@ -0,0 +1,15 @@
+import pytest
+from src.maths_oparations import add, sub
+
+def test_add():
+    assert add(2, 3) == 5
+    assert add(-1, 1) == 0
+    assert add(0, 0) == 0
+    assert add(-2, -3) == -5
+
+def test_sub():
+    assert sub(5, 3) == 2
+    assert sub(0, 0) == 0
+    assert sub(-1, -1) == 0
+    assert sub(-3, -2) == -1
+    assert sub(2, 5) == -3
